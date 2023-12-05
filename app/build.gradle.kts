a617dd9cd44b7a7ae5d565b40b850b5a1e47681d
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-05 16:00:32 +0300

    First screen user interface created.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ozcanbayram.gezle"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ozcanbayram.gezle"
--- app/src/main/java/com/ozcanbayram/gezle/FirstActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/FirstActivity.kt
@@ -3,9 +3,9 @@ package com.ozcanbayram.gezle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_first)
     }
 }
