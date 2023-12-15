5ccd2f2a26f7acecd533ea01ffa2dcded634d784
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-15 13:37:31 +0300

    Last know location

--- app/src/main/java/com/ozcanbayram/gezle/MapsActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.ozcanbayram.gezle
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -34,6 +35,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //For give permission:
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
 
+    //SharedPreferences
+    private lateinit var sharedPrefereneces : SharedPreferences
+
+    //onLocationChanged 1 kez çalrılması için:
+    private var trackBoolean : Boolean? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -47,6 +54,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         registerLauncehr()
 
+        sharedPrefereneces = this.getSharedPreferences("com.ozcanbayram.gezle", MODE_PRIVATE)
+        trackBoolean = false
+
     }
     override fun onMapReady(googleMap: GoogleMap) { //Harita hazı olduğunda çalışacak metot.
         mMap = googleMap
@@ -59,7 +69,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             override fun onLocationChanged(location: Location) { //Konum değiştiği zaman bize verilecek location(içerisinde Location tutan parametre)
                 //Bu metodu tanımladıktan sonra kullanabilmek için kullanıcıdan konum izni almalıyız.
                 //Need Permission
-                println("User Location: " + location.toString())
+
+                trackBoolean = sharedPrefereneces.getBoolean("trackBoolean",false)
+                if(!trackBoolean!!){
+                    val userLocation = LatLng(location.latitude,location.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f)) //sürekli onLocationChanged çağrılırsa haritada rahat gezemeyiz
+                    //bu yüzden sharedPreferences kullanarak bunun sadece 1 kez çağrılmasını sağlayabiliriz.
+                    sharedPrefereneces.edit().putBoolean("trackBoolean",true).apply() //TrackBoolean artık true olur ve bir daha çalışmaz
+                }
+                
+
             }
 
 
@@ -100,6 +119,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             // kaç saniyede bir  konum güncellemesini alacağını milisaniy cinsinden belirt, kaç metreede bir alınacağını float cinsinden belirt.,
             // [locationListener: buradan gelen verileri global tanımlı konum dinleyicisine ata(Yuakrıda kullanılan locationListener)]) -->
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener) //needing location permission
+
+            //Kullanıcının son konumunu alma
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+            }
+            mMap.isMyLocationEnabled = true //Konumum alındı mı doğrulaması
         }
 
 
@@ -135,6 +162,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     // kaç saniyede bir  konum güncellemesini alacağını milisaniy cinsinden belirt, kaç metreede bir alınacağını float cinsinden belirt.,
                     // [locationListener: buradan gelen verileri global tanımlı konum dinleyicisine ata(Yuakrıda kullanılan locationListener)]) -->
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1f,locationListener) //needing location permission
+
+                    //Kullanıcının son konumunu alma
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (lastLocation != null){
+                        val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                    }
+                    mMap.isMyLocationEnabled = true //Konumum alındı mı doğrulaması
                 }
 
             }else{
