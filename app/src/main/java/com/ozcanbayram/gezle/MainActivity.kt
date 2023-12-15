0facaa083be982ba57f483417f28053353527250
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-15 19:07:16 +0300

    Added menu in MainActivity

--- app/src/main/java/com/ozcanbayram/gezle/MainActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.ozcanbayram.gezle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { //Menunyu aktiviteyle bagla
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { //Menuden eleman secilirse ne olacak
+        if(item.itemId == R.id.add_place){
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
