793d72a7d9398e415bbbc007c954553f6ca909b8
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-16 18:39:33 +0300

    LoginActivity is created

--- app/src/main/java/com/ozcanbayram/gezle/Login.kt
+++ app/src/main/java/com/ozcanbayram/gezle/Login.kt
@@ -3,27 +3,21 @@ package com.ozcanbayram.gezle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ozcanbayram.gezle.databinding.ActivityLoginBinding
 import com.ozcanbayram.gezle.databinding.ActivityWelcomeBinding
 
-class Welcome : AppCompatActivity() {
-    private lateinit var binding : ActivityWelcomeBinding
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.kaydol.setOnClickListener {
-            val intent = Intent(this,Register::class.java)
-            startActivity(intent)
-            finish()
-        }
-
         binding.giris.setOnClickListener {
-            val intent = Intent(this,Login::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
         }
-
     }
 }
--- app/src/main/java/com/ozcanbayram/gezle/Register.kt
+++ app/src/main/java/com/ozcanbayram/gezle/Register.kt
@@ -2,10 +2,14 @@ package com.ozcanbayram.gezle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+    fun register(view : View){
+
     }
 }
