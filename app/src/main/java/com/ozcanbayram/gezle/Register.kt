2afa08181b70a50eaaadeaa759b0a06c23327774
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-18 22:01:04 +0300

    Sign up is complated

--- app/src/main/java/com/ozcanbayram/gezle/Register.kt
+++ app/src/main/java/com/ozcanbayram/gezle/Register.kt
@@ -1,15 +1,53 @@
 package com.ozcanbayram.gezle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.ozcanbayram.gezle.databinding.ActivityRegisterBinding
+import com.ozcanbayram.gezle.databinding.ActivityWelcomeBinding
 
 class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    //For Fİrebase Sign Up
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
     }
     fun register(view : View){
+        val email  = binding.email.text.toString()
+        val password = binding.pasword.text.toString()
+        val ad_soyad = binding.adSoyad.text.toString()
+
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(this,"Lütfen E-posta ve Şifre bilgilerinizi giriniz.",Toast.LENGTH_LONG).show()
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                //Successfull
+                val intent = Intent(this@Register,Login::class.java)
+                Toast.makeText(this@Register,"Harika! Gezlemek için giriş yapabilirsin.",Toast.LENGTH_LONG).show()
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                //Fail
+                Toast.makeText(this@Register,it.localizedMessage,Toast.LENGTH_LONG).show() //Başarısız olursa mesaj göster
+            }
+
+
+        }
+
 
     }
 }
