c0f668c5ad06c277683a655aece65bb19049097c
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-19 19:12:15 +0300

    Current user controll in FirstActivity

--- app/src/main/java/com/ozcanbayram/gezle/FirstActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/FirstActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ozcanbayram.gezle.databinding.ActivityFirstBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class FirstActivity : AppCompatActivity() {
     private lateinit var binding : ActivityFirstBinding
+    //For Firebase
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_first)
@@ -18,5 +23,15 @@ class FirstActivity : AppCompatActivity() {
             val intent = Intent(this,Welcome::class.java)
             startActivity(intent)
         }
+
+        auth=Firebase.auth
+
+        val currentUser = auth.currentUser
+        if(currentUser!=null){
+            val  intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
