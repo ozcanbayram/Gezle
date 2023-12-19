815192d24b58299397f558bdb23ede8a0179f6f6
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-19 20:04:55 +0300

    Sign out complated

--- app/src/main/java/com/ozcanbayram/gezle/MainActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/MainActivity.kt
@@ -6,18 +6,25 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.ozcanbayram.gezle.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.ozcanbayram.gezle.databinding.ActivityMapsBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    //For Fİrebase
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean { //Menunyu aktiviteyle bagla
@@ -30,6 +37,12 @@ class MainActivity : AppCompatActivity() {
         if(item.itemId == R.id.add_place){
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
+        }
+        if(item.itemId == R.id.sign_out){
+            auth.signOut()
+            val intent = Intent(this,FirstActivity::class.java)
+            startActivity(intent)
+            finish()
         }
         return super.onOptionsItemSelected(item)
     }
