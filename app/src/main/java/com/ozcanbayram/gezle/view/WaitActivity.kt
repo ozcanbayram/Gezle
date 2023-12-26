9e3505c0d03fcb3fdf774b4e3124822179cb458a
ozcanbayram <ozzcanbayram@gmail.com>
2023-12-26 21:10:59 +0300

    WaitActivity is complated

--- app/src/main/java/com/ozcanbayram/gezle/view/WaitActivity.kt
+++ app/src/main/java/com/ozcanbayram/gezle/view/WaitActivity.kt
@@ -1,12 +1,34 @@
 package com.ozcanbayram.gezle.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.ozcanbayram.gezle.R
+import com.ozcanbayram.gezle.databinding.ActivityDetailsBinding
+import com.ozcanbayram.gezle.databinding.ActivityWaitBinding
 
 class WaitActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityWaitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wait)
+        binding = ActivityWaitBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // 3 saniye sayma işlemini başlat
+        val sayac = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Sayma işlemi devam ediyor
+            }
+
+            override fun onFinish() {
+                // Sayma işlemi bitti, başka bir aktiviteye git
+                val intent = Intent(this@WaitActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        sayac.start()
+
     }
 }
